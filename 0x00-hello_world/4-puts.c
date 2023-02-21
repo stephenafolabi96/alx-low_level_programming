6fbd20fada550a70be9b216092d784ab8fd8ee4a
Stephen <stephenafolabi96@gmail.com>
2023-02-20 17:21:07 -0800

    4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - A programme that prints a lne wth puts functions 
- *Return: 0 (when programme returns zero it show sucess)
+ * main - Entry point
+ * Return:Always 0 (successs)
  */
 int main(void)
 {
- puts("\"Programming is like building a multilingual puzzle"");
- return (0); 
+puts("\"Programming is like building a multilingual puzzle");
+return (0); 
 }
 
