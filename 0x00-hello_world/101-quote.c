58d028d347090ad31474ea984a95fd4a9becac0b
stephen afolabi <stephenafolabi96@gmail.com>
2023-02-21 15:45:57 +0100

    101-quote.c)

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A c code that prints a line to the standard of unix operating
+ * systmem, to a new line and standard erro.
+ * Return: 1 (success)
+ */
+int main(void)
+{
+	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\r", stdout);
+	return (1);
+}
