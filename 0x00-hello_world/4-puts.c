2fb76b43a9db06e8a72ec7c0fe44fa5df47b2945
stephen afolabi <stephenafolabi96@gmail.com>
2023-02-21 01:27:59 +0100

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,11 @@
-#!/bin/bash
-gcc $FILE -o cisfun
+#include <stdio.h>
+/**
+ * main - A programme that prints a lne wth puts functions 
+ *Return: 0 (when programme returns zero it show sucess)
+ */
+int main(void)
+{
+ puts("\"Programming is like building a multilingual puzzle"");
+ return (0); 
+}
+
