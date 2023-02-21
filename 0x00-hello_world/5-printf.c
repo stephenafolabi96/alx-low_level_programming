4ff5ca88f1aa96afe1c6d29ed12e770a4aea4a7d
Stephen <stephenafolabi96@gmail.com>
2023-02-20 17:43:23 -0800

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints exactly with proper grammar,
+ * but the outcome is a piece of art,, followed by a new line.
+ * Return: 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n")
+		return (0);
+}
